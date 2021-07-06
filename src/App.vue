3ebebd18a4b8bf2af1c043a2d8b1e4ca810c3c13
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-07-07 00:22:20 +0900

    added boards

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,51 @@
         <b-form-input v-model="newTask" placeholder="Enter Task" @keyup.enter="add"></b-form-input>
       </div>
     </div>
-    <div class="row">
+    <div class="row mt-3">
       <div class="col-md-3">
-        <div class="p-2 alert alert-secondary">
+        <div class="p-2 alert alert-primary">
+          <h3>To Do</h3>
+          <draggable class="list-group" :list="arrToDo" group="tasks">
+            <div class="list-group-items" v-for="element in arrToDo" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+       <div class="col-md-3">
+        <div class="p-2 alert alert-warning">
           <h3>Backlog</h3>
-          <draggable class="list-group" :list="arrBacklog" group="tasks"></draggable>
+          <draggable class="list-group" :list="arrBacklog" group="tasks">
             <div class="list-group-items" v-for="element in arrBacklog" :key="element.name">
               {{element.name}}
             </div>
+          </draggable>
         </div>
       </div>
+
+       <div class="col-md-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>In Progress</h3>
+          <draggable class="list-group" :list="arrInProgress" group="tasks">
+            <div class="list-group-items" v-for="element in arrInProgress" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+       <div class="col-md-3">
+        <div class="p-2 alert alert-success">
+          <h3>Done</h3>
+          <draggable class="list-group" :list="arrDone" group="tasks">
+            <div class="list-group-items" v-for="element in arrDone" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -30,14 +65,14 @@ export default {
   data() {
     return {
       newTask: "",
-      arrBacklog: [
-        {name: "Code Sign Up Page"},
-        {name: "Test Dashboard"},
-        {name: "Style Registration"},
-        {name: "Help with Designs"}
+      arrToDo: [
+        {name: "Code"},
+        {name: "Test"},
+        {name: "Style"},
+        {name: "Push"}
       ],
+      arrBacklog: [],
       arrInProgress: [],
-      arrTested: [],
       arrDone: []
     }
   },
