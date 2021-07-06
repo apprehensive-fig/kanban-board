4f4097588e52c8a09fa427c209931f2a3947839b
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-07-06 23:58:25 +0900

    set up draggable

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable "vuedraggable";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    draggable
+  },
+  data() {
+    return {
+      newTask: "",
+      arrBacklog: [
+        {name: "Code Sign Up Page"}
+        {name: "Test Dashboard"}
+        {name: "Style Registration"}
+        {name: "Help with Designs"}
+      ],
+      arrInProgress: [],
+      arrTested: [],
+      arrDone: []
+    }
+  },
+  methods: {
+    add() {
+      if(this.newTask) {
+        this.arrBacklog.push({name: this.newTask});
+        this.newTask= "";
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
+import draggable "vuedraggable";
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
